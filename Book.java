db77f3be83483790a3fa1c16605f9ac589b4a2cb
Jay <ryder@Jays-MacBook-Air.local>
2024-08-10 03:37:32 +0530

    Create Book Class with Constructor and getter methods

---
+++ Book.java
@@ -0,0 +1,37 @@
+public class Book {
+
+    private String isbn;
+    private String title;
+    private String author;
+    private int pubYear;
+    private int totCopies;
+    private int borrowedCopies;
+
+    public Book(String isbn, String title, String author, int publishyear, int totalCopies) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.pubYear = publishyear;
+        this.totCopies = totalCopies;
+        this.borrowedCopies = 0;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getPubYear() {
+        return pubYear;
+    }
+    public int getTotalCopies() {
+        return totCopies;
+    }
+    public int getBorrowedCopies() {
+        return borrowedCopies;
+    }
+}
